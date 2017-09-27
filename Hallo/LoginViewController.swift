4d4a243f1d2565014458be4ccb6dff0e24575a9f
Carina Boo <carinaboo@berkeley.edu>
2017-09-27 15:27:46 -0700

    loginviewcontroller setup. does not push chatcontroller yet.

--- Hallo/LoginViewController.swift
+++ Hallo/LoginViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +28,52 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onSignUp(_ sender: UIButton) {
+        // Initialize a user object
+        let newUser = PFUser()
+        
+        // Set user properties
+        newUser.username = emailTextField.text
+        newUser.password = passwordTextField.text
+        
+        // Call sign up function on the object
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+                // Show error alert
+                let alertController =  UIAlertController(title: "Sign up failed", message: "Might already have been registered", preferredStyle: .alert)
+                let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: { (action: UIAlertAction) in
+                    //
+                })
+                alertController.addAction(cancelAction)
+                self.present(alertController, animated: true, completion: nil)
+            } else {
+                print("User Registered successfully")
+                // manually segue to logged in view
+            }
+        }
+    }
+    
+    @IBAction func onLogIn(_ sender: UIButton) {
+        let username = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User log in failed: \(error.localizedDescription)")
+                // Show error alert
+                let alertController =  UIAlertController(title: "Log in failed", message: "Wrong username/password", preferredStyle: .alert)
+                let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: { (action: UIAlertAction) in
+                    //
+                })
+                alertController.addAction(cancelAction)
+                self.present(alertController, animated: true, completion: nil)
+            } else {
+                print("User logged in successfully")
+                // display view controller that needs to shown after successful login
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
